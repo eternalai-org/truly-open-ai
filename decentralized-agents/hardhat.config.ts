655108879bc3e5bb5abd47ca023f761f6077b8d1
eai-dev <dev@eternalai.org>
2025-01-10 15:26:35 +0700

    Add initial decentralized agents, decentralized storage and ai-kernel project files and configuration

---
+++ decentralized-agents/hardhat.config.ts
@@ -0,0 +1,109 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+import "@matterlabs/hardhat-zksync";
+import "@nomiclabs/hardhat-solhint";
+import "hardhat-deploy";
+import "dotenv/config";
+import "hardhat-contract-sizer";
+
+let localTestMnemonic =
+  "test test test test test test test test test test test junk";
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: { enabled: true, runs: 2000000 },
+          viaIR: true,
+          evmVersion: "paris",
+        },
+      },
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: { enabled: true, runs: 2000000 },
+          viaIR: true,
+          evmVersion: "paris",
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: localTestMnemonic,
+        accountsBalance: "10000000000000000000000000",
+      },
+      gas: 100_000_000,
+      allowUnlimitedContractSize: true,
+      blockGasLimit: 1_000_000_000_000,
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      accounts: {
+        mnemonic: localTestMnemonic,
+        count: 10,
+      },
+      timeout: 500_000_000,
+      gas: 90_000_000,
+      blockGasLimit: 2_500_000_000,
+    } as any,
+    abstract_testnet: {
+      url: "https://api.testnet.abs.xyz",
+      chainId: 11124,
+      accounts: [process.env.ABSTRACT_TESTNET_PRIVATE_KEY],
+      allowUnlimitedContractSize: true,
+      ethNetwork: "https://testnet.runechain.com/rpc", // The Ethereum Web3 RPC URL.
+      zksync: true,
+      gasPrice: "auto",
+    } as any,
+    base_mainnet: {
+      url:
+        "https://base-mainnet.infura.io/v3/" +
+        process.env.BASE_MAINNET_INFURA_API_KEY,
+      chainId: 8453,
+      accounts: [process.env.BASE_MAINNET_PRIVATE_KEY],
+      allowUnlimitedContractSize: true,
+      ethNetwork: "https://testnet.runechain.com/rpc", // The Ethereum Web3 RPC URL.
+      zksync: false,
+      gasPrice: "auto",
+    } as any,
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  paths: {
+    sources: "./smart-contracts",
+    tests: "./tests",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  etherscan: {
+    apiKey: {
+      regtest3: "abc123",
+    },
+    customChains: [
+      {
+        network: "regtest3",
+        chainId: 20156,
+        urls: {
+          apiURL: "https://eternal-ai3.tc.l2aas.com/api",
+          browserURL: "https://eternal-ai3.tc.l2aas.com",
+        },
+      },
+    ],
+  },
+  mocha: {
+    timeout: 2000000,
+    color: true,
+    reporter: "mocha-multi-reporters",
+    reporterOptions: {
+      configFile: "./mocha-report.json",
+    },
+  },
+};
+
+export default config;
---
+++ decentralized-storage/smart-contracts/standardUpgradeable/interfaces/IBase.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IGPUManager {
+    function getMinFeeToUse(uint32 modelId) external view returns (uint256);
+}
+
+interface IInferable {
+    function infer(
+        uint32 modelId,
+        bytes calldata data,
+        address creator
+    ) external returns (uint64 inferenceId);
+
+    function infer(
+        uint32 modelId,
+        bytes calldata data,
+        address creator,
+        bool flag
+    ) external returns (uint64 inferenceId);
+}
