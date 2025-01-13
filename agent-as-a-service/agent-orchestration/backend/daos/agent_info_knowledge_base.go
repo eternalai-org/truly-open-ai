87237a73f0eff1d6f63936f757a9e0171d6ce827
770102 <770102>
2025-01-13 12:16:15 +0700

    add api agent use knowledge base

--- agent-as-a-service/agent-orchestration/backend/daos/agent_info_knowledge_base.go
+++ agent-as-a-service/agent-orchestration/backend/daos/agent_info_knowledge_base.go
@@ -15,3 +15,34 @@ func (dao *DAO) FirstAgentInfoKnowledgeBaseByAgentInfoID(tx *gorm.DB, agentInfoI
 	}
 	return &agentInfoKnowledgeBase, nil
 }
+
+func (dao *DAO) FirstAgentInfoKnowledgeBaseByAgentInfoIDAndKnowledgeBaseID(tx *gorm.DB, agentInfoID uint,
+	knowledgeBaseID uint) (*models.AgentInfoKnowledgeBase, error) {
+	var agentInfoKnowledgeBase models.AgentInfoKnowledgeBase
+	if err := dao.first(tx, &agentInfoKnowledgeBase, map[string][]interface{}{
+		"agent_info_id = ?":     {agentInfoID},
+		"knowledge_base_id = ?": {knowledgeBaseID},
+	}, nil, nil, false); err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &agentInfoKnowledgeBase, nil
+}
+
+func (dao *DAO) CreateAgentInfoKnowledgeBase(tx *gorm.DB, agentInfoKnowledgeBase *models.AgentInfoKnowledgeBase) error {
+	return dao.Create(tx, agentInfoKnowledgeBase)
+}
+
+// ========== KNOWLEDGE BASE ==========
+func (dao *DAO) FirstKnowledgeBase(tx *gorm.DB, filters map[string][]interface{}, preload map[string][]interface{}, order []string, forUpdate bool) (*models.KnowledgeBase, error) {
+	var knowledgeBase models.KnowledgeBase
+	if err := dao.first(tx, &knowledgeBase, filters, preload, order, forUpdate); err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &knowledgeBase, nil
+}
