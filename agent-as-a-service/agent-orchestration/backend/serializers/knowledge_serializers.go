5c8a1192f92c816003f2acd49edef0ead3a02cf1
9593 <9593>
2025-01-14 11:08:43 +0700

    update model

--- agent-as-a-service/agent-orchestration/backend/serializers/knowledge_serializers.go
+++ agent-as-a-service/agent-orchestration/backend/serializers/knowledge_serializers.go
@@ -24,21 +24,23 @@ type File struct {
 }
 
 type KnowledgeBase struct {
-	ID                   uint                 `json:"id"`
-	CreatedAt            time.Time            `json:"created_at"`
-	UpdatedAt            time.Time            `json:"updated_at"`
-	Status               int64                `json:"status"`
-	UserAddress          string               `json:"user_address"`
-	DepositAddress       string               `json:"deposit_address"`
-	SolanaDepositAddress string               `json:"solana_deposit_address"`
-	DepositTxHash        string               `json:"deposit_tx_hash"`
-	Name                 string               `json:"name"`
-	Description          string               `json:"description"`
-	AgentId              string               `json:"agent_id"`
-	ResultUrl            string               `json:"result_url"`
-	NetworkID            uint64               `json:"network_id"`
-	Fee                  float64              `json:"fee"`
-	KnowledgeBaseFiles   []*KnowledgeBaseFile `json:"knowledge_base_files"`
+	ID                     uint                 `json:"id"`
+	CreatedAt              time.Time            `json:"created_at"`
+	UpdatedAt              time.Time            `json:"updated_at"`
+	Status                 int64                `json:"status"`
+	UserAddress            string               `json:"user_address"`
+	DepositAddress         string               `json:"deposit_address"`
+	SolanaDepositAddress   string               `json:"solana_deposit_address"`
+	DepositTxHash          string               `json:"deposit_tx_hash"`
+	Name                   string               `json:"name"`
+	Description            string               `json:"description"`
+	AgentId                string               `json:"agent_id"`
+	ResultUrl              string               `json:"result_url"`
+	NetworkID              uint64               `json:"network_id"`
+	Fee                    float64              `json:"fee"`
+	KnowledgeBaseFiles     []*KnowledgeBaseFile `json:"knowledge_base_files"`
+	KBTokenContractAddress string               `json:"kb_token_contract_address"`
+	KBTokenID              string               `json:"kb_token_id"`
 }
 
 type KnowledgeBaseFile struct {
