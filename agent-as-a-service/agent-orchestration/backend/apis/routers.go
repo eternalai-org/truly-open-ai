5902940cbb32e31be8f909e6b64a6d4765ad2e4f
0x1755 <>
2025-01-15 15:54:15 +0700

    fix

--- agent-as-a-service/agent-orchestration/backend/apis/routers.go
+++ agent-as-a-service/agent-orchestration/backend/apis/routers.go
@@ -227,7 +227,7 @@ func (s *Server) Routers() {
 			// Token management
 			tokenAPI := internalAPI.Group("/trade")
 			{
-				tokenAPI.GET("tokens", s.GetAgentTradeTokens)
+				tokenAPI.GET("/tokens", s.GetAgentTradeTokens)
 				tokenAPI.GET("/search", s.DexSearchPair)
 				tokenAPI.GET("/trade-history/latest", s.DexPairsTradeLatest)
 				tokenAPI.GET("/solana/chart-24h/:mint", s.GetSolanaDataChart24Hour)
