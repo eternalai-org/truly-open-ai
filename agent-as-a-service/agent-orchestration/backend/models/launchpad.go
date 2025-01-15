5938c56cddfe65fd22810f3a3dcb71ea2c58107a
0xaf16 <>
2025-01-15 15:13:59 +0700

    update for launchpad

--- agent-as-a-service/agent-orchestration/backend/models/launchpad.go
+++ agent-as-a-service/agent-orchestration/backend/models/launchpad.go
@@ -1,18 +1,22 @@
 package models
 
 import (
+	"time"
+
 	"github.com/jinzhu/gorm"
 )
 
 type LaunchpadStatus string
 
 const (
-	LaunchpadStatusNew LaunchpadStatus = "new"
-	LaunchpadStatusEnd LaunchpadStatus = "end"
+	LaunchpadStatusNew     LaunchpadStatus = "new"
+	LaunchpadStatusRunning LaunchpadStatus = "running"
+	LaunchpadStatusEnd     LaunchpadStatus = "end"
 )
 
 type Launchpad struct {
 	gorm.Model
+	NetworkID              uint64
 	TwitterPostID          uint   `gorm:"unique_index"`
 	TweetId                string `gorm:"unique_index"`
 	Name                   string
@@ -25,6 +29,8 @@ type Launchpad struct {
 	Status                 LaunchpadStatus
 	AgentSnapshotMissionID uint
 	AgentSnapshotMission   *AgentSnapshotMission
+	StartAt                *time.Time
+	EndAt                  *time.Time
 }
 
 type LaunchpadMember struct {
