e05acc186ed732a038350c83f97c10a9175922ac
31803smith <195430354+31803smith@users.noreply.github.com>
2025-01-23 12:44:11 +0700

    add agent_contract_address to local_contracts.json

--- decentralized-compute/worker-hub/internal/model/local_chain.go
+++ decentralized-compute/worker-hub/internal/model/local_chain.go
@@ -1,19 +1,20 @@
 package model
 
 type LocalChain struct {
-	Contracts         map[string]string `json:"contracts"`
-	Rpc               string            `json:"rpc"`
-	ModelName         string            `json:"model_name"`
-	UseExternalRunPod bool              `json:"use_external_run_pod"`
-	RunPodInternal    string            `json:"run_pod_internal"` // if we use custom runpod url (not local).  RunPodInternal=RunPodExternal=url
-	RunPodExternal    string            `json:"run_pod_external"` //for heath check
-	RunPodAPIKEY      string            `json:"run_pod_api_key"`
-	ModelID           string            `json:"model_id"`
-	ChainID           string            `json:"chain_id"`
-	PrivateKey        string            `json:"private_key"`
-	Miners            map[string]Miners `json:"miners"`
-	Platform          string            `json:"platform"`
-	ApiUrl            string            `json:"api_url"`
+	Contracts            map[string]string `json:"contracts"`
+	Rpc                  string            `json:"rpc"`
+	ModelName            string            `json:"model_name"`
+	UseExternalRunPod    bool              `json:"use_external_run_pod"`
+	RunPodInternal       string            `json:"run_pod_internal"` // if we use custom runpod url (not local).  RunPodInternal=RunPodExternal=url
+	RunPodExternal       string            `json:"run_pod_external"` //for heath check
+	RunPodAPIKEY         string            `json:"run_pod_api_key"`
+	ModelID              string            `json:"model_id"`
+	ChainID              string            `json:"chain_id"`
+	PrivateKey           string            `json:"private_key"`
+	Miners               map[string]Miners `json:"miners"`
+	Platform             string            `json:"platform"`
+	ApiUrl               string            `json:"api_url"`
+	AgentContractAddress string            `json:"agent_contract_address"`
 }
 
 type Miners struct {
