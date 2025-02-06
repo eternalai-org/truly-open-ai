f49fc5351ca3a6f2cadb14dfcb3070b24a0f2a28
31803smith <195430354+31803smith@users.noreply.github.com>
2025-02-06 15:10:26 +0700

    Update code

--- decentralized-compute/worker-hub/internal/usecase/api_local_chain_v1.go
+++ decentralized-compute/worker-hub/internal/usecase/api_local_chain_v1.go
@@ -154,7 +154,7 @@ func (c *API_Local_Chain_V1) CreateInfer(ctx context.Context, request model.LLMI
 	_pBFTCommittee := make(map[string]string)
 	proposer := ""
 break_here:
-	for index < 150 {
+	for index < 15000 {
 		time.Sleep(2 * time.Second)
 		assignmentIDs, err := wkHub.GetAssignmentsByInference(nil, inferIdBig)
 		if err != nil {
