52d8061ebd3b0a8a8e786a732bef350addaa1bb7
0xaf16 <>
2025-02-07 08:50:24 +0700

    dao for agent infra

--- agent-as-a-service/agent-orchestration/backend/models/agent_infra.go
+++ agent-as-a-service/agent-orchestration/backend/models/agent_infra.go
@@ -1,1 +1,37 @@
 package models
+
+import (
+	"github.com/eternalai-org/eternal-ai/agent-as-a-service/agent-orchestration/backend/types/numeric"
+	"github.com/jinzhu/gorm"
+)
+
+type AgentInfra struct {
+	gorm.Model
+	OwnerAddress string
+	Name         string
+	Description  string           `gorm:"type:text"`
+	EaiBalance   numeric.BigFloat `gorm:"type:decimal(36,18);default:0"`
+	Icon         string           `gorm:"type:text"`
+}
+
+type (
+	AgentInfraTransactionType   string
+	AgentInfraTransactionStatus string
+)
+
+const (
+	AgentInfraTransactionTypeFee AgentInfraTransactionType = "fee"
+
+	AgentInfraTransactionStatusDone AgentInfraTransactionStatus = "done"
+)
+
+type AgentInfraTransaction struct {
+	gorm.Model
+	NetworkID    uint64
+	AgentInfraID uint   `gorm:"index"`
+	EventId      string `gorm:"unique_index"`
+	Type         AgentInfraTransactionType
+	Amount       numeric.BigFloat `gorm:"type:decimal(36,18);default:0"`
+	Toolset      string
+	Status       AgentInfraTransactionStatus
+}
