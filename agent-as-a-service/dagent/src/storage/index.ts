8ef1596db7b397c7159e8afe80c5789df9c6fb86
0x0603 <villatoto063@gmail.com>
2025-02-10 16:59:41 +0700

    _: add storage

--- agent-as-a-service/dagent/src/storage/index.ts
+++ agent-as-a-service/dagent/src/storage/index.ts
@@ -1,4 +1,5 @@
 import { LocalStorage } from "node-localstorage";
 
-export const twitterStorage = new LocalStorage('./twitter-service-db');
+const storage_path = './storage';
+export const twitterStorage = new LocalStorage(storage_path + '/twitter-service-db');
 
